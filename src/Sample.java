9b0aae9acb96f482aee3282e6e1b9843bb50e827
Yuusuke Watanabe <>
2019-11-13 11:21:59 +0900

    とりあえず枠だけ作った

--- src/Sample.java
+++ src/Sample.java
@@ -1,9 +1,14 @@
-import javax.swing.JFrame;
+import javax.swing.*;
 
 public class Sample {
+    public static final String APP_NAME = "TODOアプリ";
+    public static final String APP_VERSION = "1.0";
+
     public static void main(String[] args) {
-        JFrame frame = new JFrame("タイトル");
-        frame.setBounds(100, 100, 400, 300);
+        JFrame frame = new MenuFrame();
+        frame.setTitle(APP_NAME);
+        frame.setBounds(100, 100, 800, 600);
+        frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
     }
