658dd8851cc82864163a93c24b83a39e18f1738c
Yuusuke Watanabe <>
2019-12-16 15:31:07 +0900

    Bug(?): account.csv のどこかに"魔法の呪文"と入れると、バージョン表示ウィンドウの表示文字列が狂う

--- src/app/Application.java
+++ src/app/Application.java
@@ -6,7 +6,7 @@ import javax.swing.*;
 
 public class Application {
     public static final String NAME = "ATMアプリ";
-    public static final String VERSION = "1.0";
+    public static       String VERSION = "1.0";
     public static final String BANK_NAME = "RITS銀行";
 
     public static void main(String[] args) {
